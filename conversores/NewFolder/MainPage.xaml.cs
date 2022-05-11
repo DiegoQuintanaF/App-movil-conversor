d7d8eb369a5d2ccb89e09dd261e96b9a82f335e0
Diego <dquintana@utb.edu.co>
2022-05-10 20:07:26 -0500

    Primer commit

---
+++ conversores/NewFolder/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using conversores.NewFolder;
+
+namespace conversores
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(dolares.Text))
+            {
+                var pesos = double.Parse(dolares.Text);
+                double dolar = pesos * 4087.33;
+                Resultado.Text = pesos+" en pesos colombianos es: "+dolar.ToString();
+            }
+            else
+            {
+                DisplayAlert("Error", "no tiene valor a convertir", "OK");
+            }
+        }
+
+        private async void btcolombia_Clicked(object sender, EventArgs e)
+        {
+           Navigation.PushAsync(new convertidor_de_pesos_colombianos());
+        }
+    }
+}
---
+++ conversores/NewFolder/convertidor de pesos colombianos.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace conversores.NewFolder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class convertidor_de_pesos_colombianos : ContentPage
+    {
+        public convertidor_de_pesos_colombianos()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PesosColombianos.Text))
+            {
+                var pesos = double.Parse(PesosColombianos.Text);
+                double dolares = pesos * 0.00025;
+                Dolares.Text = pesos + " en dolares es: " + dolares.ToString();
+            }
+            else
+            {
+                DisplayAlert("Error", "no tiene valor a convertir", "OK");
+            }
+        }
+
+        private void btdolar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
